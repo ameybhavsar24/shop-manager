13d867b3b003fc1c83a3d593a0fb7bcedd757127
Amey Bhavsar <iamamey24@gmail.com>
2021-04-16 13:37:40 +0530

    format js and css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
- $(document).ready(function(){
-    $('.sidenav').sidenav({
-      draggable: true,
-    });
-    $('#modal-add-item').modal();
-    let modals_edit = $('.modal-edit');
-    modals_edit.modal();
-//     for (let i=0; i<modals_edit.length; i++) {
-//       modals_edit
-//     }
-    $('input#item_name').characterCounter();
-    $('select').formSelect();
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    draggable: true,
   });
+  $('#modal-add-item').modal();
+  let modals_edit = $('.modal-edit');
+  modals_edit.modal();
+  //     for (let i=0; i<modals_edit.length; i++) {
+  //       modals_edit
+  //     }
+  $('input#item_name').characterCounter();
+  $('select').formSelect();
+});
